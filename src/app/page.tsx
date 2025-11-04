813589df50c5b4377d2d70e46afd72463f436ce0
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-11-04 15:56:26 +0200

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { ThemeProvider } from "@/providers/themeProvider/ThemeProvider";
 import NavbarLayoutFloatingInline from '@/components/navbar/NavbarLayoutFloatingInline';
@@ -16,7 +16,7 @@ import { Award, ChefHat, Crown, Gift, Heart, HelpCircle, Leaf, MapPin, MessageSq
 export default function Home() {
   return (
     <ThemeProvider
-      defaultButtonVariant="slide-background"
+      defaultButtonVariant="text-stagger"
       defaultTextAnimation="entrance-slide"
       borderRadius="pill"
     >
